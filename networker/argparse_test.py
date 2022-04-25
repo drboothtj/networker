551920f1e59365a4d48b50b0d737a28dffacb120
DrBoothTJ <63703480+DrBoothTJ@users.noreply.github.com>
2022-04-25 14:47:29 +0200

    Update argparse_test.py

--- networker/argparse_test.py
+++ networker/argparse_test.py
@@ -1,12 +1,38 @@
 #imports
-import argparse 
-import sys
+import argparse
 
-parser = argparse.ArgumentParser("networker", description="networker:  a python package to generate and analyse protein similarity networks.", epilog="Written by Dr. Thom Booth, 2022.")
-parser.add_argument('-t', '--test', action='store_true', help='you should not see this option >:(')
+#create the parser
+parser = argparse.ArgumentParser(
+    "networker",
+    description="networker:  a python package to generate and analyse protein similarity networks.",
+    epilog="Written by Dr. Thom Booth, 2022."
+    )
+
+#create arguments
+parser.add_argument(
+    '-f',
+    '--faa',
+    nargs='?',
+    default=None,
+    help='path to a fasta file containing amino acid sequences'
+    )
+parser.add_argument(
+    '-t',
+    '--tsv',
+    nargs='?',
+    default=None,
+    help='path to a .tsv containing BLASTP results'
+    )
+parser.add_argument(
+    '-n',
+    '--node',
+    nargs='?',
+    default=None,
+    help='the name of a node for subnetwork analysis'
+    )
 
 args = parser.parse_args()
-print(args.test)
-
-
+print(args.faa)
+print(args.tsv)
+print(args.node)
     
