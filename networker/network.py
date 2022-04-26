af50ac1cd990dd51bb222f0e938f52fc5b6c021b
DrBoothTJ <63703480+DrBoothTJ@users.noreply.github.com>
2022-04-26 14:35:13 +0200

    Update network.py

--- networker/network.py
+++ networker/network.py
@@ -1,12 +1,24 @@
+'''
+Create and manipulate pyvis networks
+
+Functions:
+    get_new_network() -> network
+    plot_network(network, sources, targets, weights) -> network
+    annotate_network(network) -> network
+    get_node_list(network) -> node_list
+'''
+
 from pyvis.network import Network
 
 def get_new_network():
+    '''Create an empty network'''
     network = Network(height='100%', width='75%', bgcolor='white', font_color='black')
     network.barnes_hut()
     network.show_buttons(filter_=True)
     return network
 
 def plot_network(network, sources, targets, weights):
+    '''Plot nodes to a network using a list of sources, targets and weights'''
     edge_data = zip(sources, targets, weights)
     for edge in edge_data:
         source = edge[0]
@@ -17,20 +29,22 @@ def plot_network(network, sources, targets, weights):
         network.add_edge(source, destination, value=weight)
     return network
 
-
 def annotate_network(network):
+    '''Colour and annotate specific nodes in a network'''
     colourme = 'BGC' ## change this to add colour definition
     neighbor_map = network.get_adj_list()
-    node_list = []
     for node in network.nodes:
-        numberofneighbours = len(neighbor_map[node['id']])
-        node['title'] += '<br>' + str(numberofneighbours) + ' neighbors:<br>' + '<br>'.join(neighbor_map[node['id']])
-        node['value'] = numberofneighbours
+        neighbours = len(neighbor_map[node['id']])
+        title = '<br>' + str(neighbours) + ' neighbors:<br>' + '<br>'.join(neighbor_map[node['id']])
+        node['title'] += title
+        node['value'] = neighbours
         if colourme in node['id']:
             node['color'] = 'red'
     return network
 
 def get_node_list(network):
+    '''Return a list of nodes from a network'''
+    node_list = []
     for node in network.nodes:
         node_list.append(node['id'])
     return node_list
